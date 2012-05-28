6ea5498ee7324cc7e1a431d3044eb8f4b93c2253
Jeremy Tice <jetpacmonkey@gmail.com>
2012-05-28 00:48:07 -0400

    Got the edit map screen displaying already created maps and selecting terrain

--- static/js/models/map.js
+++ static/js/models/map.js
@@ -3,8 +3,108 @@
 		defaults: {
 			"name": "",
 			"creator": null
+		},
+
+		getDimensions: function(cells) {
+			if (arguments.length === 0) {
+				cells = this.collection.globalCollections.cells;
+			}
+			if (!this.x || !this.y) {
+				var dim = findDimensions(this, cells);
+				this.x = dim.x;
+				this.y = dim.y;
+			}
+			return {"x": this.x, "y": this.y};
+		},
+		resize: function(newX, newY, cells) {
+			if (arguments.length <= 2) {
+				cells = this.collection.globalCollections.cells;
+			}
+
+			//double check that the stored dimensions are correct
+			this.x = this.y = null;
+			this.getDimensions(cells);
+			var oldX = this.x;
+			var oldY = this.y;
+			var oldCells = cells.where({"mapObj": this.get("id")});
+
+			var remove = [];
+			var add = [];
+
+			if (newX < oldX || newY < oldY) {
+				//remove cells with too high of an X or Y value
+				_.forEach(oldCells, function(cell) {
+					if (cell.get("x") >= newX || cell.get("y") >= newY) {
+						remove.push(cell);
+					}
+				}, this);
+			}
+			if (newX > oldX) {
+				for (var i=oldX; i<newX; ++i) {
+					for (var j=0; j<newY; ++j) {
+						add.push({"x": i, "y": j, "mapObj": this.get("id")});
+					}
+				}
+			}
+			if (newY > oldY) {
+				for (var j=oldY; j<newY; ++j) {
+					for (var i=0; i<oldX; ++i) {
+						add.push({"x": i, "y": j, "mapObj": this.get("id")});
+					}
+				}
+			}
+			cells.remove(remove);
+			cells.add(add);
+		},
+		render: function(cells) {
+			if (arguments.length === 0) {
+				cells = this.collection.globalCollections.cells;
+			}
+			var lastX = -1;
+			var mapCells = cells.where({"mapObj": this.get("id")});
+			var curCol = null;
+			var mapDiv = $("<div>").addClass("map");
+			_.each(mapCells, function(cell) {
+				var x = cell.get("x");
+				var y = cell.get("y");
+				if (x != lastX) {
+					if (curCol) {
+						mapDiv.append(curCol);
+					}
+					curCol = $("<div>").addClass("mapCol");
+					lastX = x;
+				}
+				var cellDiv = $("<div>").addClass("mapCell terrain-" + cell.get("terrain")).data({"x": x, "y": y});
+				curCol.append(cellDiv);
+			}, this);
+			if (curCol) {
+				mapDiv.append(curCol);
+			}
+			mapDiv.append("<div style='clear: both'></div>");
+
+			return mapDiv;
 		}
 	});
+
+	var findDimensions = function(map, cells) {
+		var lastCell = null;
+		var mapId = map.get("id");
+
+		//assume cells collection is sorted. This means the last cell that matches this map has the dimensions
+		for (var i=cells.length - 1; i>=0; --i) {
+			var cell = cells.at(i);
+			if (cell.get("mapObj") == mapId) {
+				lastCell = cell;
+				break;
+			}
+		}
+
+		if (lastCell) {
+			return {"x": lastCell.get("x") + 1, "y": lastCell.get("y") + 1};
+		} else {
+			return {"x": 0, "y": 0};
+		}
+	}
 
 	MapSet = BaseCollection.extend({
 		model: Map,
--- static/js/ui/CreateMapView.js
+++ static/js/ui/CreateMapView.js
@@ -7,12 +7,111 @@
 
 			this.load("createdMaps", "cells", "terrainTypes");
 
+			this.editView = new EditMapView();
+			this.selView = new SelectMapView();
+			this.editView.mainView = this.selView.mainView = this;
+
 			BaseView.prototype.initialize.apply(this);
 		}
 	});
 
 	var SelectMapView = Backbone.View.extend({
-		
+		el: ".selectMap",
+		events: {
+			"click .newMap": "newMap",
+			"click .viewMap": "viewMap"
+		},
+		render: function() {
+
+		},
+
+		newMap: function(e) {
+
+		},
+		viewMap: function(e) {
+			var mapid = $(e.currentTarget).data("mapid");
+			var map = this.mainView.createdMaps.get(mapid);
+			this.mainView.editView.model = map;
+			this.mainView.editView.render();
+		}
+	});
+
+	var TerrainSelectView = Backbone.View.extend({
+		el: ".mapTerrainTypes",
+		initialize: function() {
+			this.selected = null;
+		},
+
+		render: function() {
+			if (this.selected) {
+				this.$(".selectedTerrainTypeName").text(this.selected.get("name")).show();
+				this.$(".selectedTerrainType").attr("class", "selectedTerrainType").addClass("terrain-" + this.selected.get("id"));
+			} else {
+				this.$(".selectedTerrainTypeName").hide();
+				this.$(".selectedTerrainType").attr("class", "selectedTerrainType")
+			}
+			return this;
+		}
+	});
+
+	var EditMapView = Backbone.View.extend({
+		el: ".editMap",
+		events: {
+			"click .renameMap": "rename",
+			"click .resizeMap": "resize",
+			"click .terrainSelect": "terrainSelect",
+			"click .mapCell": "terrainSet",
+			"change #mapWidth, #mapHeight": "changeDimInput"
+		},
+		initialize: function() {
+			this.terrainView = new TerrainSelectView();
+		},
+
+		render: function() {
+			this.$(".mapCells").empty();
+			if (this.model) {
+				//properties
+				this.$(".mapName").text(this.model.get("name"));
+				var dim = this.model.getDimensions();
+				this.$("#mapWidth").val(dim.x).prop("disabled", false);
+				this.$("#mapHeight").val(dim.y).prop("disabled", false);
+				$(".renameMap").show();
+
+				//cells
+				var mapDiv = this.model.render(this.mainView.cells);
+				this.$(".mapCells").html(mapDiv);
+			} else {
+				this.$(".mapName").text("---");
+			}
+
+			return this;
+		},
+		rename: function() {
+
+		},
+		changeDimInput: function(e) {
+			$(e.currentTarget).val(Math.floor($(e.currentTarget).val()));
+
+			var oldDim = this.model.getDimensions();
+			var changed = this.$("#mapWidth").val() != oldDim.x || this.$("#mapHeight").val() != oldDim.y;
+			if (changed) {
+				this.$(".resizeMap").show();
+			} else {
+				this.$(".resizeMap").hide();
+			}
+		},
+		resize: function() {
+
+		},
+		terrainSelect: function(e) {
+			var terrainId = $(e.currentTarget).data("terrainid");
+			var terrain = this.mainView.terrainTypes.get(terrainId);
+			this.terrainView.selected = terrain;
+			this.terrainView.render();
+		},
+		terrainSet: function(e) {
+
+		}
 	});
 
 	$(function() {

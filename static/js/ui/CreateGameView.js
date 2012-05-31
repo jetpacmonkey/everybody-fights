e46c5fec279ad7a0881baa0d8b43e744bd75303c
Jeremy Tice <jetpacmonkey@gmail.com>
2012-05-31 00:10:54 -0400

    switched create game ui to using margin-left instead of left position, so it stays centered

--- static/js/ui/CreateGameView.js
+++ static/js/ui/CreateGameView.js
@@ -16,7 +16,7 @@
 			"click .settingsOption": "selectOption"
 		},
 		initialize: function() {
-			this.initLeft = this.$el.position().left;
+			this.initLeftMargin = parseInt(this.$el.css("margin-left"), 10);
 			this.areaInitLeft = this.$(".settingsArea:eq(1)").css("left");
 			this.areaWidth = this.$(".settingsArea").outerWidth();
 		},
@@ -29,17 +29,15 @@
 				nextArea.css("left", self.areaInitLeft).show().animate({"left": 0}, {
 					"duration": 500,
 					"complete": function() {
-						var startLeft = self.$el.position().left;
-						self.$el.css("left", startLeft);
-						self.$el.animate({"left": "-=" + self.areaWidth + "px"}, 200, "linear");
+						self.$el.animate({"margin-left": "-=" + self.areaWidth + "px"}, 200, "linear");
 					}
 				});
 			} else {
 				//next area already showing, so we had already selected something here.
 				//deselect everything to the right and fade out choices farther to the right than the immediate next area
-				nextArea.nextAll().find(".selected").removeClass("selected");
+				nextArea.nextAll().add(nextArea).find(".selected").removeClass("selected");
 				nextArea.nextAll(":visible").fadeOut(200).promise().done(function() {
-					self.$el.animate({"left": self.initLeft - self.areaWidth * (self.$(".settingsArea:visible").length - 1)}, 300, "linear");
+					self.$el.animate({"margin-left": self.initLeftMargin - self.areaWidth * (self.$(".settingsArea:visible").length - 1)}, 300, "linear");
 				});
 			}
 		}

843ac652688c8ec762a5447b2da4e7e16bc5adb1
Jeremy Tice <jetpacmonkey@gmail.com>
2012-11-02 22:21:35 -0400

    Getting closer to AI being ready
    Little by little...

--- server/fight/models/AI.py
+++ server/fight/models/AI.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.db.models import Q
+from fight.models.Game import *
+import random
+
+def adjCells(gameCell):
+	origCell = gameCell.origCell
+	x = origCell.x
+	y = origCell.y
+	return GameCell.objects.filter(
+		(Q(origCell__x=x) & Q(origCell__y=y+1) | Q(origCell__y=y-1)) |  # same column
+		((Q(origCell__x=x-1) | Q(origCell__x=x+1)) & (Q(origCell__y=y) | Q(origCell__y=y+(1 if x%2 else -1)))),
+		game=gameCell.game
+		)
 
 class AIEngine(models.Model):
 	TYPE_RAND = "random"
@@ -11,6 +24,25 @@ class AIEngine(models.Model):
 	def __unicode__(self):
 		if self.aiType == self.TYPE_RAND:
 			return "Random"
+
+	def execute(self, player, characters, all_characters):
+		if self.aiType == AIEngine.TYPE_RAND:
+			char = characters[random.randint(0, characters.count())]  # randomly decide what character will be doing an action
+			moveCost = char.calcAttr("moveCost")
+			rngAtkCost = char.calcAttr("rangeAttackCost")
+			melAtkCost = char.calcAttr("meleeAttackCost")
+			apRemaining = player.apRemaining
+
+			adj = adjCells(char.cell)
+			openAdj = adj.filter(gamecharacter=None)
+
+			availActionTypes = []
+			if moveCost <= apRemaining and openAdj.exists():
+				availActionTypes.append("move")
+			if melAtkCost <= apRemaining and adj.exclude(openAdj).exists():
+				availActionTypes.append("melAtk")
+			if rngAtkCost <= apRemaining:
+				pass
 
 	class Meta:
 		app_label = "fight"
--- server/fight/models/UserProfile.py
+++ server/fight/models/UserProfile.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from fight.models.Game import GamePlayer, GameCharacter
 
 from fight.models.AI import AIEngine
 
@@ -10,6 +11,16 @@ class UserProfile(models.Model):
 
 	aiEngine = models.ForeignKey(AIEngine, null=True, default=None, blank=True, verbose_name="AI Engine")
 	aiSettings = models.TextField(default="{}", help_text="JSON dictionary of settings that certain AI types may need")
+
+	def doAIMove(self, game):
+		if game.gamePhase == 1:  # Buying characters
+			pass
+		elif game.gamePhase == 2:  # Main gameplay
+			player = GamePlayer.objects.get(game=game, player=self.user)
+			characters = GameCharacter.objects.filter(owner=player)
+			all_characters = GameCharacter.objects.filter(owner__game=game)
+
+			aiEngine.execute(player=player, characters=characters, all_characters=all_characters)
 
 	def __unicode__(self):
 		return "%s's profile" % self.user.username
@@ -23,6 +34,7 @@ def profileGetter(user):
 	except:
 		pro = UserProfile()
 		pro.user = user
+		pro.save()
 		return user.userprofile
 
 User.profile = property(profileGetter)

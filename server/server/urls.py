f72ae4b0a87a5e8dfbe25cede82145ae725830c1
Jeremy Tice <jetpacmonkey@gmail.com>
2013-01-28 23:55:11 -0500

    got rid of unnecessary additional piece of url
    Not sure why I had that in there to begin with, honestly...

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^fight/', include('fight.urls')),
+    url(r'^/?', include('fight.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
